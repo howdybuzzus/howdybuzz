742716a12c7ef9d804c891af2a6874704a5e4bcf
howdybuzzus <howdybuzzus@gmail.com>
2026-03-03 16:50:43 +0530

    System: Refresh Vault Bucket [A]

---
+++ vault/data-a.js
@@ -0,0 +1,12 @@
+window.vaultData = [
+  {
+    "id": 1,
+    "word": "Abate",
+    "pos": "",
+    "def": "To become less intense or widespread; to subside.",
+    "ex": "",
+    "exams": [
+      "Vocabulary"
+    ]
+  }
+];
