195d301a1570c42c1bcd665a1262d7e5dfe2d273
howdybuzzus <howdybuzzus@gmail.com>
2026-03-03 18:38:35 +0530

    Refresh Vault Bucket [A]

--- vault/data-a.js
+++ vault/data-a.js
@@ -1,12 +1,1 @@
-window.vaultData = [
-  {
-    "id": 1,
-    "word": "Abate",
-    "pos": "",
-    "def": "To become less intense or widespread; to subside.",
-    "ex": "The storm began to abate after several hours of heavy rain.",
-    "exams": [
-      "Vocabulary"
-    ]
-  }
-];
+window.vaultData = [];
